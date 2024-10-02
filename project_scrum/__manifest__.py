8d142329845289246dff895834fa5be7c46fb4cf
Ariel Barreiros <arielbarreiros96@gmail.com>
2024-10-02 19:57:08 +0100

    [16.0][ADD] project_scrum initial commit Added the structure in OCA fashion, with pre-commit and stuff

---
+++ project_scrum/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project Scrum",
+    "version": "16.0.1.0.0",
+    "summary": "Adds Scrum Workflow to projects",
+    "author": "Ariel Barreiros, Odoo Community Association (OCA)",
+    "website": "https://github.com/arielbarreiros96/project_scrum",
+    "category": "Generic",
+    "depends": ["project"],
+    "license": "AGPL-3",
+    "data": ["views/project_views.xml"],
+    "installable": True,
+}
